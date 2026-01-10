7db9bca535e98d0b3b82878fc66ce1f074e68562
Gus <guslateralus@gmail.com>
2026-01-10 16:55:57 -0600

    Exercises about operators added, let's continue with conditionals

---
+++ Fundamentos/06-exercises.js
@@ -0,0 +1,12 @@
+// 1. Evalúa si un número es par o impar:
+console.log("-----------EJERCICIO 1: PAR O IMPAR----------------------")
+const num = 4
+num % 2 === 0 ? console.log(`El número ${num} es par`) : console.log(`El número ${num} es impar`)
+
+// 2. Comparador de edades: Compara dos edades ingresadas e imprime la mayor.
+console.log("-----------EJERCICIO 2: COMPARADOR DE EDADES-----------------------")
+let edad1 = 27
+let edad2 = 36
+
+edad1 > edad2 ? console.log(`La edad de ${edad1} es la mayor`) : console.log(`La edad de ${edad2} es la mayor`)
+
